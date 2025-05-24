d3b65d8a9d7864e4b61bbc026095d17dae59ee34
MasterHiggins <christopher.higgins@keyin.com>
2025-05-24 12:50:13 -0230

    Final Release version

---
+++ Search.js
@@ -0,0 +1,42 @@
+SeasonTags = {
+    CTM1: {
+        Name: "Chris' Taskmaster 1",
+        Set: "1",
+        Season: "1",
+        Cast: ["Sterling", "Fisher", "David"],
+        Image: "assets/CTM1.png",
+        Link: "Seasons/CTM1.html"
+    },
+    CTM2: {
+        Name: "Chris' Taskmaster 2",
+        Set: "1",
+        Season: "2",
+        Cast: ["Down", "Fisher", "David"],
+        Image: "assets/CTM2.png",
+        Link: "Seasons/CTM2.html"
+    },
+    CTM3: {
+        Name: "Chris' Taskmaster 3",
+        Set: "1",
+        Season: "3",
+        Cast: ["Down", "Fisher", "Tyler"],
+        Image: "assets/CTM3.png",
+        Link: "Seasons/CTM3.html"
+    },
+    COC1: {
+        Name: "Champion of Champions 1",
+        Set: "1",
+        Season: "4",
+        Cast: ["Sterling", "Fisher", "David", "Down"],
+        Image: "assets/COC.png",
+        Link: "Seasons/COC1.html"
+    },
+    AS1: {
+        Name: "All Stars 1",
+        Set: "1",
+        Season: "5",
+        Cast: ["Sterling", "Fisher", "David", "Down", "Tyler"],
+        Image: "assets/AS.png",
+        Link: "Seasons/AS1.html"
+    }
+}
